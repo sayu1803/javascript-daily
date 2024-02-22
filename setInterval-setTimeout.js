01361ef051f107128f810c17ac436172bbb35897
sayu1803 <sayantanrabi.03@gmail.com>
2024-02-22 15:32:50 +0530

    Update setInterval-setTimeout.js

--- setInterval-setTimeout.js
+++ setInterval-setTimeout.js
@@ -1,10 +1,10 @@
 alert("hello")
-let a = setTimeout(function(e){
+let b = setTimeout(function(e){
     alert("world");
 },5000);
 
 
 //to clear timeout
 
-clearTimeout(a)
-console.log(a)
+clearTimeout(b)
+console.log(b)
