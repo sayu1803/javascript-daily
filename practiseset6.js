4278c1fdd30461ce054bc88711a900ac5eb7ea93
sayu1803 <sayantanrabi.03@gmail.com>
2024-02-23 22:53:32 +0530

    23 feb

--- practiseset6.js
+++ practiseset6.js
@@ -6,18 +6,5 @@ if(age>=18){
   alert("you can drive")
 }else{
   alert("you can not drive")
-}
+}   
 
-//q2-codewith-harry-vid-29
-
-let agee=prompt("enter the age:")
-agee=Number.parseInt
-let runAgain=true
-while(
-  if(agee >=18){
-    alert("you can drive")
-  }else{
-    alert("you can not drive")
-  }
-  runAgain=confirm("do you want to run again")
-)
